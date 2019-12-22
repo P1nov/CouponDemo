0719b17e8eac673a97cc2ed57d864daee12c82ad
P1no <1473781785@qq.com>
2019-12-22 14:10:23 +0800

    Initial Commit

---
+++ UITestDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITestDemo
+//
+//  Created by ma c on 2019/12/22.
+//  Copyright © 2019 ma c. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
